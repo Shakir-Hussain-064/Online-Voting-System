0d3e5290542b8068c9838857cdbf5ad2f4d05630
Shakir Hussain <shakirhusain2021@gmail.com>
2026-04-26 18:37:11 +0530

    deployeing

--- server/server.js
+++ server/server.js
@@ -31,10 +31,15 @@ app.use((err, _req, res, _next) => {
   res.status(500).json({ message: "Internal Server Error" });
 });
 
+app.use(cors({
+  origin: "https://your-frontend.onrender.com",
+  credentials: true
+}));
+
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on https://online-voting-system-ptpr.onrender.com/`);
   });
 });
